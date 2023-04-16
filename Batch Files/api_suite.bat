71823cf465bc1fa10a42fb174fec571902be633b
paras-personal <p.thakkar200@gmail.com>
2023-04-16 13:22:27 +0100

    first commit

---
+++ Batch Files/api_suite.bat
@@ -0,0 +1,4 @@
+cd..
+
+pip install -r requirements.txt
+py.test -m "Apisuite" --html=ApiReport.html --self-contained-html --disable-warnings
---
+++ PageObjects/ApiSuite.py
@@ -0,0 +1,8 @@
+class ApiSuite:
+
+    def get_pokemon_names_from_api(self):
+        """
+        This method will fetch pokemon names from api
+        :return: string
+        """
+        print("paras")
