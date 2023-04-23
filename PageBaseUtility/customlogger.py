15d264e7e840703f3fd436f0f579fd240ff5972f
paras-personal <p.thakkar200@gmail.com>
2023-04-23 12:26:26 +0100

    Adding code to manipulate html report and other fixes

--- PageBaseUtility/customlogger.py
+++ PageBaseUtility/customlogger.py
@@ -1,6 +1,7 @@
 import inspect
 import logging
 import os
+from datetime import date
 
 
 def customLogger(logLevel=logging.DEBUG):
@@ -11,7 +12,8 @@ def customLogger(logLevel=logging.DEBUG):
         log_folder_path = os.path.join(os.path.abspath(__file__ + '/../../'), 'Logs')
         if not os.path.exists(log_folder_path):
             os.makedirs(log_folder_path)
-        fileHnadler = logging.FileHandler("Automation".format(loggerName), mode='w', encoding='utf-8')
+        log_file_path = os.path.join(log_folder_path, f'Logs_{date.today()}.log')
+        fileHnadler = logging.FileHandler(log_file_path, mode='a', encoding='utf-8')
         fileHnadler.setLevel(logLevel)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
         fileHnadler.setFormatter(formatter)
--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
-
+import pytest
+from py.xml import html
 
 
 def pytest_configure(config):
@@ -6,3 +7,36 @@ def pytest_configure(config):
     pytest hook that execute before the actual execution starts
     """
     print("Start point")
+
+
+def pytest_html_report_title(report):
+    """
+    modifying the title  of html report
+    """
+    report.title = "API TEST REPORT"
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th('Description'))
+    cells.pop(3)
+    cells.pop()
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    try:
+        cells.insert(1, html.td(report.description))
+        cells[0], cells[1] = cells[1], cells[0]
+    except AttributeError:
+        pass
+    cells.pop(2)
+    cells.pop()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    outcome = yield
+    report = outcome.get_result()
+    setattr(item, "rep_" + report.when, report)
+    report.description = str(item.function.__doc__)
